636bce91776b53dfef769f06b66453e9c0e1a371
Dave Van Wagner <git@davevw.com>
2025-02-21 21:38:04 -0800

    @angular/cli@19.1.8 @angular/core@19.1.7 @esbuild@0.25.0

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -7,9 +7,10 @@ interface Temperature {
 };
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 export class AppComponent {
   constructor(private http: HttpClient) {}
